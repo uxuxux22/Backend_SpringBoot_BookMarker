06e9a806d8b070a70402d8482f7b746dbef25265
mysoyul <vega2k_@naver.com>
2024-05-20 13:55:42 +0900

    flyway 적용

--- src/main/java/com/bookmarker/api/runner/DatabaseRunner.java
+++ src/main/java/com/bookmarker/api/runner/DatabaseRunner.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
