0142970ff20bd8fce041546d126548ff88121f08
mysoyul <vega2k_@naver.com>
2024-05-20 13:11:03 +0900

    BookmarksDTO add

--- src/main/java/com/bookmarker/api/controller/BookmarkController.java
+++ src/main/java/com/bookmarker/api/controller/BookmarkController.java
@@ -1,10 +1,12 @@
 package com.bookmarker.api.controller;
 
 import com.bookmarker.api.domain.Bookmark;
+import com.bookmarker.api.dto.BookmarksDTO;
 import com.bookmarker.api.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,7 +17,7 @@ import java.util.List;
 public class BookmarkController {
     private final BookmarkService bookmarkService;
     @GetMapping
-    public List<Bookmark> getBookmarks() {
-        return bookmarkService.getBookmarks();
+    public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
+        return bookmarkService.getBookmarks(page);
     }
 }
