54fc109df71830fb3a429e9d5578839516af3e44
mysoyul <vega2k_@naver.com>
2024-05-21 09:10:30 +0900

    bookmark insert add

--- src/main/java/com/bookmarker/api/dto/BookmarkDTO.java
+++ src/main/java/com/bookmarker/api/dto/BookmarkDTO.java
@@ -1,10 +1,13 @@
 package com.bookmarker.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 @AllArgsConstructor
@@ -12,5 +15,10 @@ public class BookmarkDTO {
     private Long id;
     private String title;
     private String url;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+                pattern = "yyyy-MM-dd HH:mm:ss",
+                timezone = "UTC")
     private Instant createdAt;
+    //private LocalDateTime createdAt;
 }
