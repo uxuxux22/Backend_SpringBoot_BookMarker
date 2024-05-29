7628a1d162ae04c2f0268fd386a5d5452b773953
spring.jeongwon <spring.jeongwon@gmail.com>
2024-05-29 21:59:11 +0900

    backend initial commit

---
+++ src/main/java/com/bookmarker/BookMarkerAPI/BookMarkerApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookmarker.BookMarkerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarkerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMarkerApiApplication.class, args);
+	}
+
+}
