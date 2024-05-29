4aa0df7d5df122ae7b1b507de55537297fedc6cc
spring.jeongwon <spring.jeongwon@gmail.com>
2024-05-30 00:58:52 +0900

    Pageable add

--- src/main/java/com/bookmarker/api/controller/BookmarkController.java
+++ src/main/java/com/bookmarker/api/controller/BookmarkController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,7 +19,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public List<Bookmark> getBookmarks() {
-        return bookmarkService.getBookmarks();
+    public List<Bookmark> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
+        return bookmarkService.getBookmarks(page);
     }
 }
--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -3,6 +3,9 @@ package com.bookmarker.api.service;
 import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
@@ -14,8 +17,9 @@ public class BookmarkService {
     private final BookmarkRepository repository;
 
     @Transactional(readOnly = true)  // 조회할때는 읽기 전용 조회메서드 으로 하여 속도에유
-    public List<Bookmark> getBookmarks() {
-        return repository.findAll();  //조회
+    public List<Bookmark> getBookmarks(Integer page) {
+        int pageNo = page < 1 ? 0 : page - 1;  // jpa가 페이지번호를 0부터
+        Pageable pageable = PageRequest.of(pageNo, 5, Sort.Direction.DESC, "id");
+        return repository.findAll(pageable).getContent();  //조회
     }
-
 }
